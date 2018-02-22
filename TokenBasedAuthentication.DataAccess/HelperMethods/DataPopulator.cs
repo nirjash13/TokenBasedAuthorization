b0a181c4fab4699900d33d70df552717e77221ee
safkat <safkat@aplectrum.com>
2018-02-22 22:49:17 +0600

    added file. removed dlls

---
+++ TokenBasedAuthentication.DataAccess/HelperMethods/DataPopulator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using TokenBasedAuthentication.DataAccess.HelperModels;
+
+namespace TokenBasedAuthentication.DataAccess.HelperMethods
+{
+    public class DataPopulator
+    {
+        public static List<AuthenticationRole> GetRoles()
+        {
+            var list = new List<AuthenticationRole>
+            {
+                new AuthenticationRole
+                {
+                    CompanyId = 1,
+                    RoleId = 1,
+                    RoleName = "Administrator"
+                },
+                new AuthenticationRole
+                {
+                    CompanyId = 1,
+                    RoleId = 2,
+                    RoleName = "Admin"
+                }
+            };
+
+
+            return list;
+        }
+    }
+}
